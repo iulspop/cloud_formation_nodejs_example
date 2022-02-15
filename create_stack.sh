97ccb52d0a919daab2fc3e9e1df5dc798d9e3056
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-02-14 23:45:31 -0500

    Add instance profile that enables access to SSM

--- create_stack.sh
+++ create_stack.sh
@@ -2,7 +2,8 @@ stack_name=fullstack-node
 
 aws cloudformation create-stack --stack-name $stack_name \
   --template-body file://fullstack-node.yaml \
-  --parameters ParameterKey=UserDataFile,ParameterValue=$(base64 -w0 setup_node_server.sh)
+  --parameters ParameterKey=UserDataFile,ParameterValue=$(base64 -w0 setup_node_server.sh) \
+  --capabilities CAPABILITY_NAMED_IAM
 
 echo -e "\nWaiting for stack creation to complete to show stack outputs..."
 aws cloudformation wait stack-create-complete --stack-name $stack_name
