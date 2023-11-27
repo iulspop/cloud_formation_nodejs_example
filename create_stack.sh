1b84abf708a82f270e9c39d1acf955e1b422feed
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-11-27 12:35:13 -0500

    make create_stack macos compatible

--- create_stack.sh
+++ create_stack.sh
@@ -1,8 +1,17 @@
 stack_name=fullstack-node
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    encoded_string=$(base64 -w0 setup_node_server.sh)
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    encoded_string=$(base64 -i setup_node_server.sh)
+else
+    echo "Unsupported operating system"
+    exit 1
+fi
+
 aws cloudformation create-stack --stack-name $stack_name \
   --template-body file://fullstack-node.yaml \
-  --parameters ParameterKey=UserDataFile,ParameterValue=$(base64 -w0 setup_node_server.sh) \
+  --parameters ParameterKey=UserDataFile,ParameterValue=$encoded_string \
   --capabilities CAPABILITY_NAMED_IAM
 
 echo -e "\nWaiting for stack creation to complete to show stack outputs..."
