293e90b49726e443db0ebe7ec3e42fac8e07a79b
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-11-27 12:22:16 -0500

    update readme and init key script

--- ssh/init_key_pair.sh
+++ ssh/init_key_pair.sh
@@ -1,5 +1,33 @@
-key_name=NodeServerKey
+key_name=EC2NodeServerKey
+
+# Create .ssh directory if it doesn't exist
+if [ ! -d ~/.ssh ]; then
+    mkdir -p ~/.ssh
+    if [ $? -ne 0 ]; then
+        echo "Failed to create .ssh directory."
+        exit 1
+    fi
+fi
+
+# Create .pem file
 touch ~/.ssh/$key_name.pem
+if [ $? -ne 0 ]; then
+    echo "Failed to create .pem file."
+    exit 1
+fi
+
+# Create EC2 key pair and write to the .pem file
 aws ec2 create-key-pair --key-name $key_name --query "KeyMaterial" --output text > ~/.ssh/$key_name.pem
+if [ $? -ne 0 ]; then
+    echo "Failed to create EC2 key pair."
+    exit 1
+fi
+
+# Change permissions of the .pem file
 chmod 400 ~/.ssh/$key_name.pem
-echo "Key pair created succesfully and private key saved in ~/.ssh/"
+if [ $? -ne 0 ]; then
+    echo "Failed to set permissions on .pem file."
+    exit 1
+fi
+
+echo "Key pair created successfully and private key saved in ~/.ssh/"
